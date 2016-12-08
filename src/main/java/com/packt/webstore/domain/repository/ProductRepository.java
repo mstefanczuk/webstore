dc5d01791844607cc6752c22685c89ec30f830d2
Michał Stefańczuk <michal.stefanczuk@gmail.com>
2016-12-08 20:04:12 +0100

    Implemented ProductController method which searchs products by filter

--- src/main/java/com/packt/webstore/domain/repository/ProductRepository.java
+++ src/main/java/com/packt/webstore/domain/repository/ProductRepository.java
@@ -3,9 +3,12 @@ package com.packt.webstore.domain.repository;
 import com.packt.webstore.domain.Product;
 
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 public interface ProductRepository {
     List<Product> getAllProducts();
     Product getProductById(String productId);
     List<Product> getProductsByCategory(String category);
+    Set<Product> getProductsbyFilter(Map<String, List<String>> filterParams);
 }
--- src/main/java/com/packt/webstore/service/ProductService.java
+++ src/main/java/com/packt/webstore/service/ProductService.java
@@ -3,8 +3,11 @@ package com.packt.webstore.service;
 import com.packt.webstore.domain.Product;
 
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 public interface ProductService {
     List<Product> getAllProducts();
     List<Product> getProductsByCategory(String category);
+    Set<Product> getProductsByFilter(Map<String, List<String>> filterParams);
 }
